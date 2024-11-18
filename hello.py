8ee76980e3f44f2a8da0963beb274beee59e3147
Michal Gorski <m.gorski@kainos.com>
2024-11-18 09:08:55 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 import random
+import requests
+from bs4 import BeautifulSoup
+
 print("Hello World")
 
 numbers = {}
 
-count = 100000
+count = 100
 sum =0
 for x in range(count):
     rand = random.randint(1,6)
@@ -15,3 +18,23 @@ for x in range(count):
 print(sum/count)
 print("------------------")
 print(dict(sorted(numbers.items())))
+
+
+url = 'https://weather.com/weather/tenday/l/93409ef628e2eccc8fe84493beb24470c722d12ef4632a9c49250af345ba81ef'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+mydivs = soup.find_all("div")
+
+for div in mydivs:
+        classes = div.get('class')
+        if classes:
+            filtered_classes = [cls for cls in classes if cls.startswith('Daily')]
+            if filtered_classes:
+                
+                print(f"Class: {filtered_classes}")
+
+
+
+
+print(soup.p['class'])
