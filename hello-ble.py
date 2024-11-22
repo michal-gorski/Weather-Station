63f43702e2167e75cefd798e35dfa41cf5ae926e
Michal Gorski <m.gorski@kainos.com>
2024-11-22 09:35:04 +0100

    Update hello-ble.py

--- hello-ble.py
+++ hello-ble.py
@@ -1,25 +1,36 @@
 import asyncio
-import bleak
+from bleak import BleakScanner, BleakClient
+
+import sys
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+# note:
+#   The default configuration of the devices is not connectable by the bluetooth HCI interface in linux.
+#   Use a tool such as https://gist.github.com/mironovdm/cb7f47e8d898e9a3977fc888d990e8a9 to increase the
+#       default HCI timeout and the devices will be connectable.
+
 
 async def main():
-    devices = await bleak.BleakScanner.discover(8)
-    for device in devices:
-        print(f"{device.name} ({device.address})")
+    devices = await BleakScanner.discover(timeout=10)
+    for d in devices:
+        print(d)
+        print(d.rssi)
+        if d.name.startswith("ATC_1CF396"):
+            print("Connecting to", d)
+            client = BleakClient(d, timeout=30)
+            try:
+                await client.connect()
+                print("connected!")
 
-# Run the main function
-#asyncio.run(main())
+                for service in client.services:
+                    for char in service.characteristics:
+                        print(char)
+            finally:
+                print("Disconnecting from", d)
+                await client.disconnect()
 
-address = "A4:C1:38:1C:F3:96"
-TEMP_ID = "00002a1f-0000-1000-8000-00805f9b34fb"
+            break
 
-async def readData(address):
-    async with bleak.BleakClient(address) as client:
-        if (not client.is_connected):
-            raise "client not connected"
-        services = await client.get_services()
-
-        temp_bytes = await client.read_gatt_char(TEMP_ID)
-        print('temp',temp_bytes)    
-
-asyncio.run(readData(address))
-
+asyncio.run(main())
