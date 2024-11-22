88bab893b6f3a7fc113b9da56128b84cf3d0b489
Michal Gorski <m.gorski@kainos.com>
2024-11-22 09:46:12 +0100

    Update hello-ble.py

--- hello-ble.py
+++ hello-ble.py
@@ -11,9 +11,10 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 #   Use a tool such as https://gist.github.com/mironovdm/cb7f47e8d898e9a3977fc888d990e8a9 to increase the
 #       default HCI timeout and the devices will be connectable.
 
+TEMP_ID = '00002a1f-0000-1000-8000-00805f9b34fb'
 
 async def main():
-    devices = await BleakScanner.discover(timeout=10)
+    devices = await BleakScanner.discover(timeout=30)
     for d in devices:
         print(d)
         print(d.name)
@@ -27,6 +28,10 @@ async def main():
                 for service in client.services:
                     for char in service.characteristics:
                         print(char)
+
+                temp_bytes = await client.read_gatt_char(TEMP_ID)
+                print('temp',temp_bytes)    
+
             finally:
                 print("Disconnecting from", d)
                 await client.disconnect()
